344e113c2401e1c83bd9237c086cfe07ae05ce82
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2024-01-11 23:00:02 +0530

    Add files via upload

---
+++ basicFetch.js
@@ -0,0 +1,21 @@
+// function fakeStore (){
+//     fetch ('https://fakestoreapi.com/products/1')
+//     .then(res=>res.json())
+//     .then(json=>console.log(json))
+// }
+
+// fakeStore()
+
+
+// async function fakeStore(){
+//     let res = await fetch('https://fakestoreapi.com/products/1');
+//     let data = await res.json();
+//     console.log(data);
+// } 
+// fakeStore()
+
+
+function callFake(){
+    let response = fetch ('https://fakestoreapi.com/products/1')
+
+}
